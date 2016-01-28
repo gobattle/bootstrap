e7addab7e6e885b604510e3bc614e9abc0d37f19
gobattle <wangtaizhou@gmail.com>
2016-01-28 20:37:14 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,16 +22,3 @@ sudo apt-get install git -y
 sudo apt-get install tmux -y
 sudo apt-get install zsh -y
 
-# customize
-
-adduser gobattle
-adduser gobattle sudo
-su gobattle
-
-sudo cd ~
-sh -c "$(curl -fsSL https://raw.github.com/gobattle/oh-my-zsh/master/tools/install.sh)"
-
-# for ycm
-sudo apt-get install build-essential cmake python-dev
-cd ~/.vim/bundle/YouCompleteMe
-./install.py
