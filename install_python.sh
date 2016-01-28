c08948c29c4488b87f39f9043657247773293805
gobattle <wangtaizhou@gmail.com>
2016-01-28 20:05:22 +0800

    update

---
+++ install_python.sh
@@ -0,0 +1,14 @@
+# install python 
+apt-get install -y gcc-multilib g++-multilib libffi-dev libffi6 libffi6-dbg python-crypto python-mox3 python-pil python-ply libssl-dev zlib1g-dev libbz2-dev libexpat1-dev libbluetooth-dev libgdbm-dev dpkg-dev quilt autotools-dev libreadline-dev libtinfo-dev libncursesw5-dev tk-dev blt-dev libssl-dev zlib1g-dev libbz2-dev libexpat1-dev libbluetooth-dev libsqlite3-dev libgpm2 mime-support netbase net-tools bzip2
+
+cd /tmp
+wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+tar xfz Python-2.7.11.tgz
+cd Python-2.7.11/
+./configure
+make
+sudo make install
+
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
+sudo easy_install pip
+pip install virtualenv
