987a44a08667163cbcea293412c8a6869297c269
gobattle <wangtaizhou@gmail.com>
2016-01-28 20:44:28 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # modify ubuntu source
 sudo cp /etc/apt/sources.list /etc/apt/sources.list_backup
@@ -13,7 +14,7 @@ deb-src http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted univer
 deb-src http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse 
 deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
-"
+" > /etc/apt/source.list
 sudo apt-get update
 
 sudo apt-get install zsh -y
