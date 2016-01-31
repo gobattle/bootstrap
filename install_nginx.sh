9b4e8fb12cf78c1d1cadc1506f0564ef1ff2223e
gobattle <wangtaizhou@gmail.com>
2016-02-01 00:29:39 +0800

    update

---
+++ install_nginx.sh
@@ -0,0 +1,9 @@
+set -e
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:nginx/stable
+
+sudo apt-get install build-essential python-dev -y
+sudo apt-get install nginx -y
+
+sudo pip install uwsgi
